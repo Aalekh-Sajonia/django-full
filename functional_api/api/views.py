54ac86b120ef57d2943b159309d8f38d1c0a9b87
Aalekh-Sajonia <aalekhbh@gmail.com>
2022-02-28 01:02:58 +0530

    APIView class

--- functional_api/api/views.py
+++ functional_api/api/views.py
@@ -4,11 +4,14 @@ from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import status
+from rest_framework.views import APIView
+
 
 # Create your views here.
 @api_view()
 def hello_world(request):
     return Response({"msg": "Aalekh"})
+
 
 # send status code by default takes 200 OK status code
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
@@ -18,7 +21,7 @@ def student_api(request, pk=None):
         if id is not None:
             stu = Student.objects.get(id=id)
             serializer = StudentSerializer(stu)
-            return Response(serializer.data, status = status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         stu = Student.objects.all()
         serializer = StudentSerializer(stu, many=True)
         return Response(serializer.data)
@@ -32,6 +35,36 @@ def student_api(request, pk=None):
         return Response(serialzer.errors)
 
     if request.method == 'PUT':
+        id = request.data.get('id')
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg": "updated"})
+
+        return Response(serializer.errors)
+
+
+class StudentApi(APIView):
+    def get(self, request, pk=None, format=None):
+        id = pk
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serialzer = StudentSerializer(data=request.data)
+        if serialzer.is_valid():
+            serialzer.save()
+            return Response({"msg": "data saved"})
+
+        return Response(serialzer.errors)
+
+    def put(self, request, format=None):
         id = request.data.get('id')
         stu = Student.objects.get(id=id)
         serializer = StudentSerializer(stu, data=request.data, partial=True)
--- functional_api/functional_api/urls.py
+++ functional_api/functional_api/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', views.hello_world),
     path('student/', views.student_api),
-    path('student/<int:pk>', views.student_api)
+    path('student/<int:pk>', views.student_api),
+    path('student_class/', views.StudentApi.as_view()),
+    path('student_class/<int:pk>', views.StudentApi.as_view())
 ]
